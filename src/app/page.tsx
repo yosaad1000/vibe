ea53f64331dd829dad9c70a25c8fb304cff93110
SAAD SAYED <110733821+yosaad1000@users.noreply.github.com>
2025-12-28 19:03:04 +0400

    feat: integrate TRPC with React Query and add initial API setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-
-
-const Page = () => {
-  
-  return(
-    <div className="font-bold" >
-      Hello World
-    </div>
-  )
+import { caller } from '@/trpc/server';
+export default async function Home() {
+  const greeting = await caller.helloo({ text: "John" });
+    //  ^? { greeting: string }
+  return <div>{greeting.greeting}</div>;
 }
-export default Page;
